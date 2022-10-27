69f4d62cbdf4f4301f382dc4c59dbf8011e3db7b
Nick Weng <nick.weng@outlook.com>
2022-10-27 23:30:10 +1100

    solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...conditions) {
+    const myArray = array;
+    const test = conditions;
+    var newArray = []; 
 
+    for (i=0; i<myArray.length; i++){
+        if (!conditions.includes(myArray[i])){
+            newArray.push(myArray[i]);
+        }
+    }
+
+    return newArray
 };
 
 // Do not edit below this line
