74c703849556b3f62d4fa9070a23cdcd3b08c1b5
Nick Weng <nick.weng@outlook.com>
2022-10-27 22:27:26 +1100

    solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const myArray = Array.from(word);
+    const arrayLength = myArray.length();
+    var reverse = "";
+
+    for (i=arrayLength; i>0; i--){
+        reverse = reverse + myArray[i];
+    }
+
+    return reverse;
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
