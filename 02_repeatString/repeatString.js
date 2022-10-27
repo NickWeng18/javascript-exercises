7db2030f54a7df4b15001093975e17002d62e75e
Nick Weng <nick.weng@outlook.com>
2022-10-27 20:53:42 +1100

    solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    var repeats =""
+    if (times<0){
+        repeats = "ERROR"
+    }
 
+    for (i=0; i<times;i++){
+        repeats = repeats + word;
+    }
+    return repeats
 };
 
 // Do not edit below this line
